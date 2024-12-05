0a97a778af9bb0db33ba4a3e099e09b7c23c3779
samon3lla <sfonsecavallejo@gmail.com>
2024-12-04 17:46:09 -0800

    done with lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,11 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - How to Play Rock, Paper, Scissors, (shoot!)
+// Sam Fonseca <sfonsec2@ucsc.edu
+// December 2024
 
+// How to Play Rock, Paper, Scissors, (shoot!)
+  //Start with 2 players 
+    //each player close fist and rest over palm 
+  //Hit palm with fist, on a beat, 4x. On each beat players will say the same words, and have synchronized movements
+    //On the first beat, both player will say Rock
+    //On the second beat, both players will say Paper
+    //On the third beat, both players will say Scissors
+    //On the 4th beat both players will say shoot 
+  //On the 4th beat, both players will say Shoot and simultaneously decide & make the hand motion for either rock, paper, or scissors
+    //The rock hand motion is a closed fist 
+    //The paper hand motion is a flat palm 
+    //The scissors hand motion is a horizontal peace sign 
+  //The selected motion will decide who wins that round
+    //The rock hand motion beats scissors
+      //The rock hand motion bangs gently on the hand that chose scissors
+    //The scissors motion beats paper
+     //The scissors hand motion will pretend to “cut” the paper 
+    //The paper beats rock
+     //The paper hand motion will envelope the rock hand motion 
+  //If players draw the same hand motion, that round is a draw. Players may decide to play rounds of three to decide the ultimate winner. 
+
+  
 // Constants
-
 // Functions
-
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
