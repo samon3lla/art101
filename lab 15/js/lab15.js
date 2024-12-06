8800bf2e7a0bfc48a36e5f52912015432d8951ef
samon3lla <sfonsecavallejo@gmail.com>
2024-12-06 06:43:35 -0800

    basic layouts created

--- lab 15/js/lab15.js
+++ lab 15/js/lab15.js
@@ -1,1 +1,17 @@
-.js
+// index.js - Lab 15: AJAX
+// Author: Sam Fonseca-Vallejo
+
+// Constants
+
+// Functions
+
+
+
+function main() {
+  console.log("Main function started.");
+
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
---
+++ lab 16/js/lab16.js
@@ -0,0 +1,17 @@
+// index.js - Lab 16: JSON and API's
+// Author: Sam Fonseca-Vallejo
+
+// Constants
+
+// Functions
+
+
+
+function main() {
+    console.log("Main function started.");
+  
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
